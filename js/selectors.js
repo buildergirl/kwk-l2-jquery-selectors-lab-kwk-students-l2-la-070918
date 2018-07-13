5760edfaae24b9560024d15f7efac9e7a015cae1
buildergirl <aprilruthowens@gmail.com>
2018-07-13 21:13:59 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -13,7 +13,7 @@ return $('.box5 #box4 a')
 }
 
 function imageSelector(selector) {
-  return document.SelectorAll('image.cat-sleeping')
+  $('image.cat-sleeping')
 }
 
 function checkboxInputSelector(selector){
